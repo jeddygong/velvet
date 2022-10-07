e5e7b7fc436da19d2591d892cbf3eb49a9daa178
gongjin <jed.gong.nine@gmail.com>
2022-10-07 16:08:19 +0800

    feat(docs): 完善 ui docs

---
+++ docs/.vitepress/config.ts
@@ -0,0 +1,54 @@
+export default {
+  base: '/',
+  title: 'velvet-UI',
+  lang: 'zh-CN',
+  // description: '',
+  themeConfig: {
+    logo: '/logo.png',
+    // repo: 'jeddygong/velvet',
+    // docsRepo: 'jeddygong/velvet',
+    // docsBranch: 'main/docs',
+
+    lastUpdated: true,
+    lastUpdatedText: '最后修改时间',
+    socialLinks: [{ icon: 'github', link: 'https://github.com/jeddygong/velvet' }],
+    editLinks: true,
+    editLink: {
+      pattern: 'https://github.com/jeddygong/velvet/main/docs/:path',
+      text: '为此页提供修改建议',
+    },
+    nav: [
+      { text: '指南', link: '/guild/installation' },
+      { text: '组件', link: '/components/button/' },
+    ],
+    // 侧边栏
+    sidebar: {
+      '/guild/': [
+        {
+          text: '基础',
+          items: [
+            {
+              text: '安装',
+              link: '/guild/installation',
+            },
+            {
+              text: '快速开始',
+              link: '/guild/quickstart',
+            },
+          ],
+        },
+      ],
+      '/components/': [
+        {
+          text: '基础组件',
+          items: [
+            {
+              text: 'Button按钮',
+              link: '/components/button/',
+            },
+          ],
+        },
+      ],
+    },
+  },
+};
